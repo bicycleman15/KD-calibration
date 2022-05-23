2db0a7c14a759a77a5b85ae6e448450e649b07c7
kadarsh22 <kadarsh22@gmail.com>
2022-05-23 20:48:30 +0530

    add `resnet34`

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
-from .resnet import resnet18, resnet50, resnet110, resnet152
+from .resnet import resnet18, resnet34, resnet50, resnet110, resnet152
 # from .resnet import resnet20, resnet56
 # from .resnet_tinyimagenet import resnet34, resnet50
 
 model_dict = {
     # resnet models can be used for cifar10/100, svhn
     "resnet18" : resnet18,
+    "resnet34" : resnet34,
     "resnet50" : resnet50,
     "resnet110" : resnet110,
     "resnet152" : resnet152
